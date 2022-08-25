f27bdd24f491c2cdcf9add716fdbc17df5790293
Code Sandbox <hello@codesandbox.io>
2022-08-25 16:29:07 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6dea0301ec25e417e9ac82340ebae776.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9688f35c7e8dc4fd0fe7027bd4f6663",
+    "url": "https://.github.io/csb-fyl2jl/index.html"
+  },
+  {
+    "revision": "b1f4a951a22ab5472bc5",
+    "url": "https://.github.io/csb-fyl2jl/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "9153f751662b13516864",
+    "url": "https://.github.io/csb-fyl2jl/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "2a3bf69b49fea8142a1a",
+    "url": "https://.github.io/csb-fyl2jl/static/css/main.7c14e544.chunk.css"
+  },
+  {
+    "revision": "a29c51abe223057fdbd6",
+    "url": "https://.github.io/csb-fyl2jl/static/js/2.ef3debb8.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "https://.github.io/csb-fyl2jl/static/js/2.ef3debb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f4a951a22ab5472bc5",
+    "url": "https://.github.io/csb-fyl2jl/static/js/3.1c8ed7cd.chunk.js"
+  },
+  {
+    "revision": "9153f751662b13516864",
+    "url": "https://.github.io/csb-fyl2jl/static/js/4.7d6f7104.chunk.js"
+  },
+  {
+    "revision": "2a3bf69b49fea8142a1a",
+    "url": "https://.github.io/csb-fyl2jl/static/js/main.f516f725.chunk.js"
+  },
+  {
+    "revision": "7c767621aaa3597c5fc4",
+    "url": "https://.github.io/csb-fyl2jl/static/js/runtime-main.d19ae722.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "https://.github.io/csb-fyl2jl/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "https://.github.io/csb-fyl2jl/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "https://.github.io/csb-fyl2jl/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "https://.github.io/csb-fyl2jl/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "https://.github.io/csb-fyl2jl/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "https://.github.io/csb-fyl2jl/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "https://.github.io/csb-fyl2jl/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "https://.github.io/csb-fyl2jl/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "https://.github.io/csb-fyl2jl/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "https://.github.io/csb-fyl2jl/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "https://.github.io/csb-fyl2jl/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "https://.github.io/csb-fyl2jl/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "24164c91e179cecd59e5d79e748a32c0",
+    "url": "https://.github.io/csb-fyl2jl/static/media/tcg.24164c91.png"
+  }
+]);
